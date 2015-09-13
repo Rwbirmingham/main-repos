1db973cb7b8c294f8f69ba283e4505a9e75cde90
unknown <RWBIRMINGHAM@ISPL157155L.containerstore.com>
2015-09-13 15:49:35 -0500

    9/13

---
+++ Chapters/#8.3-1.rb
@@ -0,0 +1,10 @@
+#Table of contents
+  line_width = 25
+  title = ('Table Of Contents '.rjust(line_width+15))
+  chap_names = [ ('Chapter 1: Getting Started'.ljust(line_width)) , ('page  1'.rjust(line_width)) , ('Chapter 2: Numbers        '.ljust(line_width)), ('page  9'.rjust(line_width)) , ('Chapter 3: Letters        '.ljust(line_width)) , ('page 13'.rjust(line_width)) ]
+
+  puts title.to_s
+  puts ''
+  puts chap_names [0].to_s + chap_names [1].to_s
+  puts chap_names [2].to_s + chap_names [3].to_s
+  puts chap_names [4].to_s + chap_names [5].to_s
---
+++ Chapters/#8.rb
@@ -0,0 +1,8 @@
+names = ['Michele','Jack','Johnny',]
+
+puts names
+puts
+puts names[0]
+puts names[1]
+puts names[2]
+
