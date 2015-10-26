14aa58945d01771ebe266956b390226e8d804e0b
unknown <RWBIRMINGHAM@ISPL157155L.containerstore.com>
2015-10-26 09:38:50 -0500

    10/26

--- Chapters/Ch.10/test5.rb
+++ Chapters/Ch.10/test5.rb
@@ -1,29 +1,21 @@
-puts "Type in what you would like to be shuffled:"
-truff_shuff = []
+puts "Type in what you would like to be sorted:"
+dir_sort = []
 
 while true
   user_input = gets.chomp
   if user_input == ""
     break
   end  
-  truff_shuff.push user_input
+  dir_sort.push user_input
 end
 
-def bubble_sort(list)
-  return list if list.size <= 1 # already sorted
-  swapped = true
-  while swapped do
-    swapped = false
-    0.upto(list.size-2) do |i|
-      if list[i] > list[i+1]
-        list[i], list[i+1] = list[i+1], list[i] # swap values
-        swapped = true
-      end
-    end    
-  end
-
-  list
+def bubble_sort arr
+return arr if arr.length <= 1
+x = arr.pop
+less = arr.select{|m| m.downcase < x.downcase}
+more = arr.select{|m| m.downcase >= x.downcase}
+bubble_sort(less) + [x] + bubble_sort(more)
 end
 
 puts "Here is your list."
-puts bubble_sort(truff_shuff)
+puts bubble_sort(dir_sort)
