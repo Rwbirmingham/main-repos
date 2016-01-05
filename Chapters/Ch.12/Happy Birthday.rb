5e8b881a220d858e02b8a05c833c7474a0ef6854
unknown <RWBIRMINGHAM@ISPL157155L.containerstore.com>
2016-01-05 09:39:22 -0600

    1/4

---
+++ Chapters/Ch.12/Happy Birthday.rb
@@ -0,0 +1,6 @@
+
+time = Time.new # The moment we ran this code.
+time2 = time - 605524500 # One minute later.
+puts time
+puts time2
+puts Time.local((1983, 5, 31, 12, 50)time.usec)
---
+++ Chapters/Ch.12/One Billion.rb
@@ -0,0 +1,4 @@
+time = Time.local(1983, 05,31,)
+time2 = time + 1000_000_000
+
+puts time2
