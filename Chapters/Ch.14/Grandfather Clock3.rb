de988eb720466b0e47a6b3ed5fa4910a3518289f
unknown <RWBIRMINGHAM@ISPL157155L.containerstore.com>
2016-02-16 12:53:09 -0600

    2/16

--- Chapters/Ch.14/Grandfather Clock3.rb
+++ Chapters/Ch.14/Grandfather Clock3.rb
@@ -1,15 +1,15 @@
 class GrandfatherClock  
 
-    def hourly dong
-        time = Time.now.hour		
-        hours =[12, 1, 2, 3, 4, 5, 6, 7, 8 , 9 , 10, 11];
-		if time >= 13
-        hour = time - 12
-        end
-        (hours[time].to_i).times do 
-            dong.call
-        end
+  def hourly dong
+    time = Time.now.hour		
+    hours =[12, 1, 2, 3, 4, 5, 6, 7, 8 , 9 , 10, 11];
+    if time >= 13
+    hours = time - 12
     end
+    (hours[time].to_i).times do 
+      dong.call
+    end
+  end
 end
 
 dong = Proc.new do 
